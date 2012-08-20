bda6d184695f4a7001de236b0f9230b7cc76ad4c
yagee-de <github@yagee.de>
2012-08-20 12:17:28 +0200

    removed console.log

--- src/main/js/jqm-init.js
+++ src/main/js/jqm-init.js
@@ -1,6 +1,5 @@
 /*global define,window */
 define([ "jquery" ], function(jQuery) {
-  window.console.log(jQuery);
   jQuery(document).bind("mobileinit", function() {
     // Make your jQuery Mobile framework configuration changes here!
     jQuery.support.cors = true;
